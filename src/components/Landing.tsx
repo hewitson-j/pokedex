cbbc574e52bcc23ce7d2874192bc8389feb90d6b
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-08 12:44:20 -0700

    Completed landing screen.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
+import "./Landing.css";
 
 export default function Landing() {
+  const welcomeText =
+    "Welcome to Professor Applewood's Unofficial Pokédex! I'm Dexter, and I will be helping you today. The world of Pokémon is vast and full of different species of Pokémon. Here in Atamona we're no different. Press the button below to get started!";
+
   return (
-    <>
-      <h1>Professor Applewood's Unofficial Pokédex</h1>
-      <p>Welcome to Professor Applewood's Unofficial Pokédex!</p>
-      <Link title="Get Started" to={"home"}>
-        <button>Home</button>
-      </Link>
-    </>
+    <div className="landing">
+      <div className="landing-screen">
+        <div className="landing-body">
+          <div className="header logo">
+            <img src="Logo.png" className="pokeball-image" />
+            <h1>
+              Professor Applewood's
+              <br />
+              Unofficial Pokédex
+            </h1>
+          </div>
+          <p id="welcome-text">{welcomeText}</p>
+          <Link title="Get Started" to={"home"}>
+            <button id="landing-home-button">Home</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
