3aa4582cf67b4b6b4a1beead95a58fa06dd8730d
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-08 14:27:31 -0700

    Added home button to Atamona.

--- src/components/Atamona.tsx
+++ src/components/Atamona.tsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Atamona.css";
 
 export default function Atamona() {
   return (
-    <>
+    <div className="atamona">
       <h2 className="content-headers">The Region of Atamona</h2>
       <div className="atamona-content">
         <img src="Atamona.jpg" alt="Atamona Map" id="atamona-image" />
         <p>This is test text.</p>
       </div>
-    </>
+      <Link title="Home" to={"/home"}>
+        <button className="button" id="atamona-home-button">
+          Back to Home
+        </button>
+      </Link>
+    </div>
   );
 }
