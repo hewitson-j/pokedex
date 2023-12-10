5775786008a91fac484df112387aa4b85301f892
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-10 13:55:08 -0700

    Added searchbar.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
+import "./Search.css";
 
 export default function Search() {
   return (
-    <>
+    <div className="search">
       <h2 className="content-headers">Search Pokedex</h2>
+      <div className="searchbar">
+        <input type="text" id="search-input" />
+        <button id="search-button" className="buttons">
+          Search
+        </button>
+      </div>
       <Link title="Home" to={"/home"}>
-        <button className="buttons">Back to Home</button>
+        <button id="search-home-button" className="buttons">
+          Back to Home
+        </button>
       </Link>
-    </>
+    </div>
   );
 }
