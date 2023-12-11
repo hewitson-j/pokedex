dd49c7ddc376bae89d0e767de0c5b8e90f586ad4
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-11 11:07:14 -0700

    Added search type selector.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,32 @@
 import { Link } from "react-router-dom";
 import "./Search.css";
+import { useState } from "react";
 
 export default function Search() {
+  const [searchType, setSearchType] = useState("number");
+
   return (
     <div className="search">
       <h2 className="content-headers">Search Pokedex</h2>
       <div className="searchbar">
+        <select
+          className="search-select"
+          onChange={(e) => {
+            setSearchType(e.target.value);
+          }}
+        >
+          <option value={"number"}>Number</option>
+          <option value={"name"}>Name</option>
+          <option value={"type"}>Type</option>
+        </select>
         <input type="text" id="search-input" />
-        <button id="search-button" className="buttons">
+        <button
+          id="search-button"
+          className="buttons"
+          onClick={() => {
+            console.log(searchType);
+          }}
+        >
           Search
         </button>
       </div>
