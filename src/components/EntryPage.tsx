d887a6557594079f49162a097332b18e1c7ee7a1
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-16 22:49:58 -0700

    Added loading for EntryPage, adjusted styles.

--- src/components/EntryPage.tsx
+++ src/components/EntryPage.tsx
@@ -10,6 +10,13 @@ export default function EntryPage() {
   const [entries, setEntries] = useState<PokemonType[]>([]);
   const [genders, setGenders] = useState<GenderProps[]>([]);
   const [types, setTypes] = useState<TypeProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const notFoundText = [
+    "Whoops! The Pokemon you're looking for is not currently in the Pokedex! (ノ_<、) But don't worry, Rotom will bring you back if you click one of the buttons below.",
+    "Oh no! You've come to the not found page. (╯°□°）not to worry, Rotom will bring you back if you click one of the below buttons.",
+    "Uh oh! You looked for a Pokemon which isn't in the Pokedex yet! (╯︵ ┻━┻) Don't worry though, Rotom will bring you back. Click one of the buttons below.",
+  ];
 
   // Data Fetchers
   const fetchPokemonEntry = async () => {
@@ -23,6 +30,7 @@ export default function EntryPage() {
     } else {
       setEntries(data);
       console.log(data);
+      setIsLoading(false);
     }
   };
   const fetchGenderData = async () => {
@@ -33,6 +41,7 @@ export default function EntryPage() {
     } else {
       console.log(data);
       setGenders(data);
+      setIsLoading(false);
     }
   };
   const fetchTypeData = async () => {
@@ -43,6 +52,7 @@ export default function EntryPage() {
     } else {
       console.log(data);
       setTypes(data);
+      setIsLoading(false);
     }
   };
 
@@ -64,65 +74,77 @@ export default function EntryPage() {
 
   return (
     <div>
-      {entry ? (
-        <>
-          <div className="entry-page">
-            <h2>
-              #{entry.dex_id}: {entry.name}
-            </h2>
-            <img src={entry.image_url} alt={entry.name} />
+      {!isLoading ? (
+        entry ? (
+          <>
+            <div className="entry-page">
+              <h2>
+                #{entry.dex_id}: {entry.name}
+              </h2>
+              <img src={entry.image_url} alt={entry.name} />
 
-            <div className="types-body">
-              <div
-                className="types"
-                style={{ backgroundColor: `${type1?.color}` }}
-              >
-                {type1?.name}
-              </div>
-              {type2 && (
+              <div className="types-body">
                 <div
                   className="types"
-                  style={{ backgroundColor: `${type2?.color}` || "inherit" }}
+                  style={{ backgroundColor: `${type1?.color}` }}
                 >
-                  {type2?.name}
+                  {type1?.name}
                 </div>
-              )}
+                {type2 && (
+                  <div
+                    className="types"
+                    style={{ backgroundColor: `${type2?.color}` || "inherit" }}
+                  >
+                    {type2?.name}
+                  </div>
+                )}
+              </div>
+              <div className="data-body">
+                <p>
+                  <b>Height:</b>
+                  <br /> {entry.height} m
+                </p>
+                <p>
+                  <b>Weight:</b>
+                  <br /> {entry.weight} kgs
+                </p>
+                <p title={gender?.name}>
+                  <b>Genders:</b>
+                  <br /> {gender?.description}
+                </p>
+              </div>
+              <p id="entry-description">
+                <b>Description:</b>
+                <br /> {entry.description}
+              </p>
+              <div className="entry-buttons">
+                <Link title="Search" to={"/search"}>
+                  <button className="buttons">Back to Search</button>
+                </Link>
+                <Link title="Search" to={"/home"}>
+                  <button className="buttons">Back to Home</button>
+                </Link>
+              </div>
             </div>
-            <div className="data-body">
-              <p>
-                <b>Height:</b>
-                <br /> {entry.height} m
-              </p>
-              <p>
-                <b>Weight:</b>
-                <br /> {entry.weight} kgs
-              </p>
-              <p title={gender?.name}>
-                <b>Genders:</b>
-                <br /> {gender?.description}
-              </p>
-            </div>
-            <p id="entry-description">
-              <b>Description:</b>
-              <br /> {entry.description}
+          </>
+        ) : (
+          <div className="entry-page-not-found">
+            <h2>Not Found</h2>
+            <p>
+              {notFoundText[Math.floor(Math.random() * notFoundText.length)]}
             </p>
             <div className="entry-buttons">
               <Link title="Search" to={"/search"}>
                 <button className="buttons">Back to Search</button>
               </Link>
-              <Link title="Search" to={"/home"}>
+              <Link title="Home" to={"/home"}>
                 <button className="buttons">Back to Home</button>
               </Link>
             </div>
           </div>
-        </>
+        )
       ) : (
-        <>
-          <p>Not found.</p>
-          <Link title="Search" to={"/search"}>
-            <button className="buttons">Back to Search</button>
-          </Link>
-        </>
+        <h2 className="entry-loading">Loading...</h2>
       )}
     </div>
   );
