bc06e91529aece5668059b6b1c743f4c246df258
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-01-14 14:47:07 -0700

    Changed browser router to hash router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Router from "./components/Router";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/pokedex">
+      <HashRouter>
         <div className="content-body">
           <div className="screen">
             <Router />
           </div>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
